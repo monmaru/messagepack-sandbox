a1173f5916449e67e618fe36db5939a2eb536d3e
Yoshitomo Minami <minami530@gmail.com>
2018-11-11 00:57:54 +0900

    Write QR code

--- client/src/main/kotlin/com/example/App.kt
+++ client/src/main/kotlin/com/example/App.kt
@@ -2,7 +2,12 @@ package com.example
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.kittinunf.fuel.Fuel
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.client.j2se.MatrixToImageWriter
+import com.google.zxing.qrcode.QRCodeWriter
 import org.msgpack.jackson.dataformat.MessagePackFactory
+import java.nio.file.FileSystems
+import java.util.*
 
 
 fun main(args: Array<String>)  {
@@ -23,6 +28,14 @@ fun main(args: Array<String>)  {
             .body(bytes)
             .responseString()
     result.fold(success = ::println) { throw it }
+
+    val qrCodeWriter = QRCodeWriter()
+    val bitMatrix = qrCodeWriter.encode(
+            Base64.getEncoder().encodeToString(bytes),
+            BarcodeFormat.QR_CODE,
+            350, 350) // width x height
+    val path = FileSystems.getDefault().getPath("qr.png")
+    MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path)
 }
 
 enum class Color(val v: Int) {
